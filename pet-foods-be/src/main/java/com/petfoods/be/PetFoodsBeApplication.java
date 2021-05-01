8d20c0f85de39dccd4e75246ddae01c732515dca
StefanoCipriani <stefano.voce@gmail.com>
2021-05-01 15:48:31 +0200

    added configurations for run backend on kubernates

--- pet-foods-be/src/main/java/com/petfoods/be/PetFoodsBeApplication.java
+++ pet-foods-be/src/main/java/com/petfoods/be/PetFoodsBeApplication.java
@@ -1,13 +1,25 @@
 package com.petfoods.be;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PetFoodsBeApplication {
+public class PetFoodsBeApplication implements CommandLineRunner{
 
+	@Value("${spring.datasource.url}")
+	private String dbUrl;
+	
+	
 	public static void main(String[] args) {
+		
 		SpringApplication.run(PetFoodsBeApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("DB URL -> "+dbUrl );	
 	}
 
 }
